313c36a2eaa9cc8c2b72876da0a1f9dd730b9d4d
Scott Cooper <scttcper@gmail.com>
2025-09-28 12:40:37 -0700

    fix: only commit changelog.md

--- scripts/npm-release/release.config.js
+++ scripts/npm-release/release.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/github"
-  ]
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 };
